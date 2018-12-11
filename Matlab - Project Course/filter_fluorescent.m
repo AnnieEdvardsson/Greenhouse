eb340afd75a0e14d9782ea80d6fd2c200f941a2c
Your Name <you@example.com>
2018-12-11 15:55:32 +0100

    changed the filter function

--- Matlab - Project Course/filter_fluorescent.m
+++ Matlab - Project Course/filter_fluorescent.m
@@ -1,5 +1,6 @@
 function filtredSignal = filter_fluorescent(signal)
-% This function applies a filter to the measured fluorescent signal
+% This function detrends the measured fluorescent signal and applies a
+% filter to it.
 %
 % Syntax:  filtredSignal = filter_fluorescent(signal)
 %
@@ -13,17 +14,18 @@ function filtredSignal = filter_fluorescent(signal)
 % Other m-files required: signal_settings.m
 % MAT-files required: none
 %
-% November 2019; Last revision: 30-November-2019
+% November 2018; Last revision: 11-December-2018
 %------------- BEGIN CODE --------------
 %% Load settings from signal_settings.m
 s_settings.conv = signal_settings();
 
 %% Design filter
 wo = s_settings.conv.f/(s_settings.conv.fs/2);  % location of the notch, must be between 0 and 1                  
-bw = wo/0.1;                                    % bandwidth at -3 dB
+bw = wo/10;                                    % bandwidth at -3 dB
 [b,a] = iirnotch(wo,bw);                        % gives numerator and denominator coeffiecients for filter function
 
 %% Add filter to signal
+signal = detrend(signal);
 z = filter(b,a,signal);                         % filters the noisy signal, should leave noise only
 filtredSignal = signal-z;                       % Subtract the noise from original signal 
 
