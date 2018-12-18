06f0667c6451762d5746e443b8bac534c86cc5f0
Indrek <indrekk@net.chalmers.se>
2018-12-18 16:28:36 +0100

    auto push

---
+++ Matlab - Project Course/autoPush.m
@@ -0,0 +1,13 @@
+function autoPush()
+
+command = 'git add *';
+[status,cmdout] = system(command)
+
+command = 'git commit -m "auto push ';
+[status,cmdout] = system(command)
+
+command = 'git push';
+[status,cmdout] = system(command)
+
+
+end
--- Matlab - Project Course/testCMD.m
+++ Matlab - Project Course/testCMD.m
@@ -1,10 +1,3 @@
-command = 'git add *';
-[status,cmdout] = system(command)
 
-command = 'git commit -m "auto com';
-[status,cmdout] = system(command)
-
-command = 'git push';
-[status,cmdout] = system(command)
 
 %[~,cmdout] = system('dir')
